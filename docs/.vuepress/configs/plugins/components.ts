a09eb0827d389fa42cf94d68c5994ab1d86cb7a2
17TheWord <17theword@gmail.com>
2024-02-01 16:10:08 +0800

    更新组件配置项

--- docs/.vuepress/configs/plugins/components.ts
+++ docs/.vuepress/configs/plugins/components.ts
@@ -1,9 +1,12 @@
 import registerComponentsPlugin from "@vuepress/plugin-register-components";
 import { path } from "@vuepress/utils";
+import {projectComponentsDir} from '../../utils';
+
+console.log(__dirname);
 
 const componentsConfig = registerComponentsPlugin(
     {
-        componentsDir: path.resolve(__dirname, './components')
+        componentsDir: projectComponentsDir
     }
 );
 
