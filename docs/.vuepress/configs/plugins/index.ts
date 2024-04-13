cf1c5e40f2e205615e8f8579c413821b55385927
17TheWord <17theword@gmail.com>
2024-04-13 13:11:29 +0000

    :hammer: Rebuild by Writerside.

--- docs/.vuepress/configs/plugins/index.ts
+++
@@ -1,9 +0,0 @@
-import { PluginConfig } from 'vuepress'
-import docsearchConfig from './docsearch'
-import componentsConfig from './components'
-const pluginsConfig: PluginConfig = [
-    docsearchConfig,
-    componentsConfig,
-];
-
-export default pluginsConfig;
